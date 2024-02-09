a9cba6c21f766e52625b446061fc1e8e9e11b7fd
MajoLkc <lukac.1majo@gmail.com>
2024-02-09 11:54:47 +0100

    Price list update

--- src/pages/PriceList/priceList.ts
+++ src/pages/PriceList/priceList.ts
@@ -1,67 +1,102 @@
 export const priceList = [
   {
-    service: "Úprava obočia",
+    service: "Microneedling Resurfacing",
+    price: 60,
+    path: ""
+  },
+  {
+    service: "Microneedling Revitalizing",
+    price: 65,
+    path: ""
+  },
+  {
+    service: "Microneedling Anti aging",
+    price: 100,
+    path: ""
+  },
+  {
+    service: "Depilácia hornej pery",
+    price: 3,
+    path: ""
+  },
+  {
+    service: "Depilácia líca/brada",
     price: 5,
-    path: "farbenie"
+    path: ""
   },
   {
-    service: "Farbenie obočia",
-    price: 7,
-    path: "farbenie"
+    service: "Depilácia podpazušie/ruky",
+    price: 10,
+    path: ""
   },
   {
-    service: "Henna brows",
-    price: 12,
-    path: "henna"
-  },
-  {
-    service: "Farbenie rias",
-    price: 8,
-    path: "farbenie"
-  },
-  {
-    service: "Farbenie rias a obočia",
-    price: 12,
-    path: "farbenie"
-  },
-  {
-    service: "Lash lifting",
-    price: 20,
-    path: "lash_lifting"
-  },
-  {
-    service: "Laminácia obočia",
-    price: 20,
-    path: "laminacia"
-  },
-  {
-    service: "Hĺbkové čistenie pleti",
-    price: 25,
-    path: "line_repair"
-  },
-  {
-    service: "Čistenie pleti – skin scrubber",
-    price: 25,
-    path: "skin_scrubber"
-  },
-  {
-    service: "Ultrazvukové ošetrenie pleti",
-    price: 25,
-    path: "ultrazvuk"
-  },
-  {
-    service: "Masáž tváre, krku a dekoltu",
-    price: 20,
-    path: "masaz"
-  },
-  {
-    service: "Relaxačné ošetrenie pleti",
-    price: 25,
-    path: "line_repair"
-  },
-  {
-    service: "Mikromasáž očného okolia",
+    service: "Depilácia predkolenie/chrbát",
     price: 15,
-    path: "masaz"
+    path: ""
+  },
+  {
+    service: "Depilácia celé nohy",
+    price: 22,
+    path: ""
+  },
+  {
+    service: "Depilácia celý chrbát",
+    price: 25,
+    path: ""
+  },
+  {
+    service: "Líčenie denné",
+    price: 5,
+    path: ""
+  },
+  {
+    service: "Líčenie príležitostné",
+    price: 15,
+    path: ""
+  },
+  {
+    service: "Líčenie večerné",
+    price: 20,
+    path: ""
+  },
+  {
+    service: "Líčenie svadobné",
+    price: 25,
+    path: ""
+  },
+  {
+    service: "Skúška svadobného líčenia",
+    price: 20,
+    path: ""
+  },
+  {
+    service: "Svadobné líčenie + skúška líčenia",
+    price: 40,
+    path: ""
+  },
+  {
+    service: "Lepenie rias",
+    price: 10,
+    path: ""
+  },
+  {
+    service: "Lepenie rias (vlastné)",
+    price: 3,
+    path: ""
+  },
+  {
+    service: "Lepenie trsov",
+    price: 10,
+    path: ""
+  },
+  {
+    service: "Vypichnutie mílií",
+    price: 5,
+    path: ""
+  },
+  {
+    service: "Vypichnutie mílií - veľké",
+    price: 10,
+    path: ""
   },
 ]
